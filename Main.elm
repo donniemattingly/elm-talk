39315758c1da396ba7a3e6c76151cdf551abe9e2
Donnie Mattingly <donniewmattingly@gmail.com>
2018-03-18 10:54:36 -0400

    Form validation example

--- Main.elm
+++ Main.elm
@@ -1,43 +1,72 @@
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 main = 
     Html.beginnerProgram { model = model, view = view, update = update }
 
+-- main = 
+--     Html.program { init = init, model = model, view = view, update = update }
+
+
+
+-- -- Init
+
+-- init: (Model, Cmd Msg)
+-- init = 
+--     let 
+--         model = 
+--             {
 
 -- MODEL
 
-type alias Model = Int
+type alias Model = 
+    { name: String
+    , password: String
+    , passwordAgain: String
+    }
 
 model: Model
 model = 
-    0
+    Model "" "" "" 
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement | Reset
+type Msg 
+    = Name String
+    | Password String
+    | PasswordAgain String
+        
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment -> 
-            model + 1
-
-        Decrement -> 
-            model - 1
-
-        Reset -> 
-            0
-        
+        Name newName -> 
+            { model | name = newName }
+        Password newPassword -> 
+            { model | password = newPassword }
+        PasswordAgain newPasswordAgain -> 
+            { model | passwordAgain = newPasswordAgain }
+            
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , br [] []
-        , button [ onClick Reset ] [ text "Reset" ]
-        ]
+  div []
+    [ input [ type_ "text", placeholder "Name", onInput Name ] []
+    , input [ type_ "password", placeholder "Password", onInput Password ] []
+    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , viewValidation model
+    ]
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+    (color, message) =
+      if model.password == model.passwordAgain then
+        ("green", "OK")
+      else
+        ("red", "Passwords do not match!")
+  in
+    div [ style [("color", color)] ] [ text message ]
