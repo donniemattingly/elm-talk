6489962a665db73e88e095152a0315d1dc783b2a
Donnie Mattingly <donniewmattingly@gmail.com>
2018-04-04 07:46:33 -0400

    Generate Random Circles

--- Main.elm
+++ Main.elm
@@ -1,72 +1,115 @@
+module Main exposing (..)
+
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
-main = 
-    Html.beginnerProgram { model = model, view = view, update = update }
-
--- main = 
---     Html.program { init = init, model = model, view = view, update = update }
+import Html.Attributes
+import Html.Events exposing (..)
+import List
+import Random
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
--- -- Init
 
--- init: (Model, Cmd Msg)
--- init = 
---     let 
---         model = 
---             {
 
 -- MODEL
 
-type alias Model = 
-    { name: String
-    , password: String
-    , passwordAgain: String
+
+type alias Model =
+    { dieFace : Int
+    , circles : List (Svg Msg)
     }
 
-model: Model
-model = 
-    Model "" "" "" 
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 2 [], Cmd.none )
+
 
 
 -- UPDATE
 
-type Msg 
-    = Name String
-    | Password String
-    | PasswordAgain String
-        
 
-update : Msg -> Model -> Model
+type Msg
+    = Roll
+    | NewFace Int
+    | GenCircle CircleGenerator
+    | NewCircle Circle
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Name newName -> 
-            { model | name = newName }
-        Password newPassword -> 
-            { model | password = newPassword }
-        PasswordAgain newPasswordAgain -> 
-            { model | passwordAgain = newPasswordAgain }
-            
--- VIEW
+        Roll ->
+            ( model, Random.generate NewFace (Random.int 1 6) )
+
+        GenCircle generator ->
+            ( model, Random.generate NewCircle generator )
+
+        NewCircle circle ->
+            let
+                newCircles =
+                    circleToSvg circle :: model.circles
+            in
+            ( { model | circles = newCircles }, Cmd.none )
+
+        NewFace newFace ->
+            ( { model | dieFace = newFace }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type_ "text", placeholder "Name", onInput Name ] []
-    , input [ type_ "password", placeholder "Password", onInput Password ] []
-    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
+    div []
+        [ svg
+            [ width "500"
+            , height "500"
+            , viewBox "0 0 500 500"
+            , fill "white"
+            , stroke "black"
+            , strokeWidth "3"
+            , Html.Attributes.style [ ( "padding-left", "20px" ), ( "padding-top", "20px" ) ]
+            ]
+            (List.append [ rect [ x "1", y "1", width "400", height "400" ] [] ] model.circles)
+        , button [ onClick (GenCircle defaultCircleGenerator) ] [ Html.text "Roll" ]
+        ]
 
-viewValidation : Model -> Html msg
-viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+
+type alias Circle =
+    { x : Float
+    , y : Float
+    , r : Float
+    }
+
+
+type alias CircleGenerator =
+    Random.Generator Circle
+
+
+circleGenerator : Float -> Float -> Float -> Float -> Float -> Float -> Random.Generator Circle
+circleGenerator minX maxX minY maxY minR maxR =
+    Random.map3 Circle (Random.float minX maxX) (Random.float minY maxY) (Random.float minR maxR)
+
+
+defaultCircleGenerator =
+    circleGenerator 21 379 21 379 2 20
+
+
+circleToSvg : Circle -> Svg Msg
+circleToSvg circ =
+    circle [ cx (toString circ.x), cy (toString circ.y), r (toString circ.r), fill "black", fillOpacity "0.4" ] []
